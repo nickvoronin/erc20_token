a0f408382c1cad8d4c07c7dd7ba56f35183caa80
Nick Voronin <naranjadito@gmail.com>
2019-03-18 11:17:50 +0200

    Shamelessly borrow comments From here https://github.com/OpenZeppelin/openzeppelin-solidity/blob/v2.2.0/contracts/token/ERC20/ERC20.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,9 +6,9 @@ contract Erc20Compliant {
     function totalSupply() public view returns (uint256);
     function balanceOf(address tokenOwner) public view returns (uint256 balance);
     function transfer(address to, uint256 value) public returns (bool success);
-//    function transferFrom(address from, address to, uint256 tokens) public returns (bool);
-//    function approve(address spender, uint256 tokens) public returns (bool);
-//    function allowance(address tokenOwner, address spender) public returns (bool);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    function allowance(address owner, address spender) public returns (bool);
 
     // optional:
     string public name;
@@ -32,19 +32,67 @@ contract HelloWorldToken is Erc20Compliant {
         decimals = tokenDecimals;
         symbol = tokenSymbol;
     }
-    
+
+    /**
+     * @dev Total number of tokens in existence
+     */
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param owner The address to query the balance of.
+     * @return A uint256 representing the amount owned by the passed address.
+     */
     function balanceOf(address owner) public view returns (uint256) {
         return balances[owner];
     }
 
+    /**
+    * @dev Transfer token to a specified address
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    */
     function transfer(address to, uint256 value) public returns (bool success) {
         require(balanceOf(msg.sender) >= value);
         balances[msg.sender] -= value;
         balances[to] += value;
         return true;
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address owner, address spender) public returns (bool) {
+
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public returns (bool) {
+
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+
     }
 }

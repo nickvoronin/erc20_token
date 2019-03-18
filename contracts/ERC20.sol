41fe4d1e411984a568b11a91f1fe50d93504639b
Nick Voronin <naranjadito@gmail.com>
2019-03-18 10:28:58 +0200

    Implement transfer with tests

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.5.0;
 
 contract Erc20Compliant {
     // required:
-    function totalSupply() public view returns (uint256 totalSupply);
+    function totalSupply() public view returns (uint256);
     function balanceOf(address tokenOwner) public view returns (uint256 balance);
-//    function transfer(address to, uint256 tokens) public returns (bool);
+    function transfer(address to, uint256 value) public returns (bool success);
 //    function transferFrom(address from, address to, uint256 tokens) public returns (bool);
 //    function approve(address spender, uint256 tokens) public returns (bool);
 //    function allowance(address tokenOwner, address spender) public returns (bool);
@@ -32,12 +32,19 @@ contract HelloWorldToken is Erc20Compliant {
         decimals = tokenDecimals;
         symbol = tokenSymbol;
     }
+    
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
 
     function balanceOf(address owner) public view returns (uint256) {
         return balances[owner];
     }
-    
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(balanceOf(msg.sender) >= value);
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        return true;
     }
 }
